602b8db377b7f10ad966f3a60f1c5c7f183f275a
tanishmundra-codes <tanishmundra2@gmail.com>
2026-02-11 11:04:04 +0530

    page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../components/landing/Header';
 import Hero  from '../components/landing/Hero';
+import {About} from '../components/landing/About';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white dark:bg-[#1D1D1D] transition-colors duration-300">
       <Header />
       <Hero />
+      <About />
     </main>
   );
 }
